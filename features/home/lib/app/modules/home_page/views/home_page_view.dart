f577d01ee8297659b7125a107374827a0624605e
Julia Stahl <julia.stahl@kepha.com.br>
2023-02-28 18:30:52 -0300

    Altera view da home

--- features/home/lib/app/modules/home_page/views/home_page_view.dart
+++ features/home/lib/app/modules/home_page/views/home_page_view.dart
@@ -6,19 +6,33 @@ import '../controllers/home_page_controller.dart';
 
 class HomePageView extends GetView<HomePageController> {
   const HomePageView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0XFFD6FFF6).withOpacity(0.9),
       appBar: AppBar(
-        title: const Text('HomePageView'),
-        centerTitle: true,
-      ),
+          title: const Text('bingou'),
+          backgroundColor: Color(0XFF231651),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            ),
+          ]),
       body: Center(
         child: Text(
           'HomePageView is working',
           style: TextStyle(fontSize: 20),
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text('Iniciar rodada'),
+        backgroundColor: Color(0XFF4DCCBD),
+        foregroundColor: Colors.black,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
